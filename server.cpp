0179c8425db83db9723c1e7ef201f4d4e7cbbda7
shrdogood <1514873277@qq.com>
2026-08-02 20:41:46 +0800

    多线程

--- server.cpp
+++ server.cpp
@@ -1,27 +1,74 @@
-#include <winsock2.h>
+#include <thread>
 #include <iostream>
 #include <cstring>  
+#include <atomic>
+#include <winsock2.h>
+
+#define MAX_CLIENTS 5
+std::atomic<int> clientCount(0);  //当前客户端数量（线程安全）
+
+void handleClient(SOCKET clientSock, SOCKADDR_IN clientaddr)
+{
+    char buf[1024];
+    //打印客户端信息（+打印服务器端线程ID）
+    std::cout << "[Thread " << std::this_thread::get_id() << "] "
+              << "客户端端口:" << ntohs(clientaddr.sin_port) << " 已连接" << std::endl;
+
+    while(true)
+    {
+        //接收数据、阻塞等待
+        int recvRet = recv(clientSock, buf, sizeof(buf), 0);
+        if (recvRet <= 0) 
+        {
+            if(recvRet == 0)
+            {
+                std::cout << "[Thread " << std::this_thread::get_id() << "] "
+                    << "客户端主动断开连接" << std::endl;
+            }
+            else
+            {
+                std::cout << "[Thread " << std::this_thread::get_id() << "] "
+                    << "接收失败" << std::endl;
+            }
+            break;
+        }
+
+        std::cout << "[Thread " << std::this_thread::get_id() << "] "
+                << "接收来自客户端的信息: " << buf << std::endl;
+        
+        //发送数据(回显)
+        if (send(clientSock, buf, strlen(buf), 0) <= 0) 
+        {
+            std::cout << "[Thread " << std::this_thread::get_id() << "] "
+                    << "发送失败" << std::endl;
+            break;
+        }
+    }
+        
+
+    // 关闭客户端套接字并减少计数
+    closesocket(clientSock);
+    clientCount--;
+    std::cout << "[Thread " << std::this_thread::get_id() << "] "
+              << "客户端断开，当前在线数: " << clientCount << std::endl;
+}
+
 
 int main()
 {
-    SOCKET serversoc;
-    SOCKET clientsoc;
-    SOCKADDR_IN serveraddr;
-    SOCKADDR_IN clientaddr;
-    char buf[1024];
-    int len;
-
     WSADATA wsa;
     WSAStartup(MAKEWORD(2, 0), &wsa);
 
     //创建套接字
-    if((serversoc = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) <= 0)
+    SOCKET serversoc = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(serversoc == INVALID_SOCKET)
     {
         std::cout << "套接字socket创建失败" << std::endl;
         WSACleanup();
         return -1;
     }
 
+    SOCKADDR_IN serveraddr;
     //定义服务器自身的 IP + 端口信息
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(9102);
@@ -37,53 +84,42 @@ int main()
     }
 
     std::cout << "开始监听..." << std::endl;
-    if(listen(serversoc, 1) != 0)
+    if(listen(serversoc, SOMAXCONN) != 0)
     {
-        printf("监听失败!\n");
-        closesocket(serversoc); // 监听失败释放
+        std::cout << "监听失败" << std::endl;
+        closesocket(serversoc);
         WSACleanup();
         return -1;
     }
+    std::cout << "服务器启动，最大并发连接数: " << MAX_CLIENTS << std::endl;
 
-    len = sizeof(SOCKADDR_IN);
-    if((clientsoc = accept(serversoc, (SOCKADDR *)&clientaddr, &len)) <= 0)
-    {
-        std::cout << "接受连接失败" << std::endl;
-        closesocket(serversoc); // 释放服务端socket
-        WSACleanup();
-        return -1;
-    }
-    std::cout << "接受连接成功" << std::endl;
-    std::cout << "客户端端口：" << ntohs(clientaddr.sin_port) << std::endl;
 
-    //接收数据
-    while(1)
+    // 主循环：接受连接
+    while (true) 
     {
-        int recvRet = recv(clientsoc, buf, 1024, 0);
-        if(recvRet <= 0)
-        {
-            std::cout << "关闭连接" << std::endl;
-            // 客户端断开，释放两个套接字
-            closesocket(clientsoc);
-            closesocket(serversoc);
-            WSACleanup();
-            return -1;
+        SOCKADDR_IN clientaddr;
+        int len = sizeof(clientaddr);
+        SOCKET clientsoc = accept(serversoc, (SOCKADDR*)&clientaddr, &len);
+        if (clientsoc == INVALID_SOCKET) {
+            std::cout << "accept 失败，退出主循环" << std::endl;
+            break;
         }
-        std::cout << "接收来自客户端的信息:" << buf << std::endl;
-        break;
+
+        // 判断是否达到最大连接数
+        if (clientCount < MAX_CLIENTS) {
+            clientCount++;   // 增加计数
+            // 创建工作线程，detach 使其独立运行
+            std::thread(handleClient, clientsoc, clientaddr).detach();
+        } else {
+            // 拒绝连接
+            const char* msg = "服务器当前繁忙，请稍后再试。\n";
+            send(clientsoc, msg, strlen(msg), 0);
+            closesocket(clientsoc);
+            std::cout << "拒绝连接（已达到最大并发数）" << std::endl;
+        }
     }
 
-    //发送数据
-    std::cout << "输入发送给客户端的信息：" << std::endl;
-    std::cin >> buf ;
-    // 去掉 +1，不需要发送\0
-    if(send(clientsoc, buf, strlen(buf), 0) <= 0)
-    {
-        std::cout << "发送失败" << std::endl;
-    }
-
-    // ========== 程序正常结束，统一释放套接字 ==========
-    closesocket(clientsoc);
+    // 主循环退出时释放服务器套接字(实际不会执行到这里)
     closesocket(serversoc);
     WSACleanup();
     return 0;
